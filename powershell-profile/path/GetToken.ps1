635369774d13942be7ecd007b582a6bed28cb411
Charlie Batten <charlie@charliebatten.co.uk>
2023-03-27 11:58:24 +0100

    Changed token to return

--- powershell-profile/path/GetToken.ps1
+++ powershell-profile/path/GetToken.ps1
@@ -22,18 +22,7 @@ function GetToken {
     )
 
     $uri = "https://microlise-test.eu.auth0.com/authorize?response_type=$response_type&client_id=$client_id&scope=$scope&redirect_uri=$redirect_uri"
-    $headers = @{
-        "Upgrade-Insecure-Requests" = "1";
-        "User-Agent" = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Insomnia/2022.7.5 Chrome/108.0.5359.62 Electron/22.0.0 Safari/537.36"
-        "Accept" = "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9";
-        "Sec-Fetch-Site" = "none";
-        "Sec-Fetch-Mode" = "navigate";
-        "Sec-Fetch-User" = "?1";
-        "Sec-Fetch-Dest" = "document";
-        "Accept-Encoding" = "gzip, deflate, br";
-        "Accept-Language" = "en-GB";
-    }
-    $response1 = Invoke-WebRequest -Method "GET" -Uri $uri -Headers $headers -MaximumRedirection 0 -SessionVariable session -ErrorAction Ignore
+    $response1 = Invoke-WebRequest -Method "GET" -Uri $uri -MaximumRedirection 0 -SessionVariable session -ErrorAction Ignore
     $state = ($response1.Headers.Location -split "state=")[1] 
 
     $uri = "https://microlise-test.eu.auth0.com/u/login/identifier?state=$state"
@@ -46,7 +35,7 @@ function GetToken {
         "webauthn-platform-available" = "false";
         "action" = "default";
     }
-    Invoke-WebRequest -Method "POST" -Uri $uri -Headers $headers -WebSession $session -Body $body -ContentType "application/x-www-form-urlencoded" -MaximumRedirection 9 | Out-Null
+    Invoke-WebRequest -Method "POST" -Uri $uri -WebSession $session -Body $body -ContentType "application/x-www-form-urlencoded" -MaximumRedirection 9 | Out-Null
 
     $uri = "https://microlise-test.eu.auth0.com/u/login/password?state=$state"
     $body = @{
@@ -56,12 +45,11 @@ function GetToken {
         "action" = "default";
     }
     try {
-        Invoke-WebRequest -Method "POST" -Uri $uri -Headers $headers -WebSession $session -Body $body -ContentType "application/x-www-form-urlencoded" -MaximumRedirection 1 | Out-Null
+        Invoke-WebRequest -Method "POST" -Uri $uri -WebSession $session -Body $body -ContentType "application/x-www-form-urlencoded" -MaximumRedirection 1 | Out-Null
     } catch {
         return ($_ -split "access_token=" -split "&scope")[1]
     }
 }
 
 $token = GetToken -email $email -password $password
-Write-Host "Bearer $token"
-
+return "Bearer $token"
