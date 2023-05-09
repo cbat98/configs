759f74546d9a715d92216e6e52a2cb53b14c0354
Charlie Batten <charlie.batten@microlise.com>
2023-05-09 13:30:05 +0100

    Changes

--- powershell-profile/Microsoft.Powershell_profile.ps1
+++ powershell-profile/Microsoft.Powershell_profile.ps1
@@ -5,11 +5,13 @@ $env:path = "C:\Program Files\Notepad++" + ";" + $env:path
 $env:path += ";" + "$configs\powershell-profile\path"
 $env:path += ";" + (Get-Item "Env:ProgramFiles(x86)").Value + "\Git\bin"
 $env:path += ";" + "C:\Program Files\GitHub CLI\"
-  
-Set-ExecutionPolicy -scope Process bypass
-  
-oh-my-posh init pwsh --config $configs\oh-my-posh\bubbles-edited.omp.json | Invoke-Expression
-  
+
 Set-Alias grep findstr
 Set-Alias sln .\*.sln
 Set-Alias cb Set-Clipboard
+  
+Set-ExecutionPolicy -scope Process bypass
+
+Impot-Module -Name "Terminal-Icons"
+
+oh-my-posh init pwsh --config $configs\oh-my-posh\bubbles-edited.omp.json | Invoke-Expression
