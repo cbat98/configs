967e891add2093df45de547bae24f58b6ab70a2a
Charlie Batten <charlie.batten@microlise.com>
2023-05-09 12:11:54 +0100

    removed title stuff

--- powershell-profile/Microsoft.Powershell_profile.ps1
+++ powershell-profile/Microsoft.Powershell_profile.ps1
@@ -1,34 +1,11 @@
 $repos = "G:\Repos"
 $configs = "$repos\configs"
-$tools = "D:\tools"
- 
-# Functions  
-function Test-Administrator {
-    $user = [Security.Principal.WindowsIdentity]::GetCurrent();
-
-    return (New-Object Security.Principal.WindowsPrincipal $user).IsInRole([Security.Principal.WindowsBuiltinRole]::Administrator)
-}
-  
-function Set-WindowTitle {
-    $Title = ""
-    
-    if (Test-Administrator) {
-        $Title = " - Administrator"
-    }
-
-    $host.ui.RawUI.WindowTitle = $env:computername + "-" + $PID + $Title
-}
-   
-# Variables
 
 $env:path = "C:\Program Files\Notepad++" + ";" + $env:path
-
 $env:path += ";" + "$configs\powershell-profile\path"
 $env:path += ";" + (Get-Item "Env:ProgramFiles(x86)").Value + "\Git\bin"
 $env:path += ";" + "C:\Program Files\GitHub CLI\"
   
-Set-WindowTitle
-   
 Set-ExecutionPolicy -scope Process bypass
   
 oh-my-posh init pwsh --config $configs\oh-my-posh\bubbles-edited.omp.json | Invoke-Expression
