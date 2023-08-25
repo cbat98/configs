8b74c6a377f3bf9fe43223ab9001e9273715457d
Charlie Batten <charlie@charliebatten.co.uk>
2023-08-25 15:13:58 +0100

    Added code to remove old packages

--- scoop/syncPackages.ps1
+++ scoop/syncPackages.ps1
@@ -1,11 +1,42 @@
 $packages = Get-Content -Path "packages.txt"
- 
-$packageList = $packages -join " "
 
-Invoke-Expression -Command "scoop.ps1 install $packageList"
+$extraPackages = @()
+$newPackages = @()
 
-Invoke-Expression -Command "scoop.ps1 update"
+Write-Host "Getting list of installed applications`n"
 
-Invoke-Expression -Comman "scoop.ps1 update *"
+$installedPackages = Invoke-Expression "scoop.ps1 list" | Select-Object -ExpandProperty "name" 
 
-Invoke-Expression -Comman "scoop.ps1 cleanup *"
+$installedPackages | ForEach-Object {
+	Write-Host "$_"
+
+	if (!$packages.Contains($_)) {
+		$extraPackages += $_
+	} else {
+		$newPackages += $_
+	}
+}
+
+if ($extraPackages.count -gt 0) {
+	Write-Host "`nFound extra packages not in packages.txt"
+	Write-Host "`nExtra apps:"
+	Write-Host ($extraPackages -join ", ")
+
+	$removeExtraPackages = Read-Host -Prompt "`nDo you want these to be removed? (y/N)"
+	if ($removeExtraPackages -eq "y") {
+		Invoke-Expression -Command "scoop.ps1 uninstall $($extraPackages -join ' ')"
+	}
+}
+
+if ($newPackages.count -gt 0) {
+	Write-Host "`nFound packages to install" 
+	Write-Host "`nNew apps:"
+	Write-Host $newPackages
+
+	Invoke-Expression -Command "scoop.ps1 install $newPackages"
+
+	Invoke-Expression -Command "scoop.ps1 update"
+	Invoke-Expression -Comman "scoop.ps1 update *"
+
+	Invoke-Expression -Comman "scoop.ps1 cleanup *"
+}
