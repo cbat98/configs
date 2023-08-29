d5359a784c8cb7a1d4222db4d82b73cad70b98d8
Charlie Batten <charlie@charliebatten.co.uk>
2023-08-29 16:30:57 +0100

    Extracted functions to install and remove packages

--- scoop/scoopSync.ps1
+++ scoop/scoopSync.ps1
@@ -1,12 +1,5 @@
-$packages = Get-Content -Path "$PSScriptRoot\manifest.json" | ConvertFrom-Json | Select-Object -ExpandProperty "packages"
-
-function Sync-Packages {
+function Get-ExtraPackages {
 	$extraPackages = @()
-	$newPackages = @()
-
-	Write-Host "Getting list of installed applications`n"
-
-	$installedPackages = Invoke-Expression "scoop.ps1 list" | Select-Object -ExpandProperty "name" 
 
 	$installedPackages | ForEach-Object {
 		Write-Host "$_"
@@ -16,12 +9,22 @@ function Sync-Packages {
 		}
 	}
 
+	return $extraPackages
+}
+
+function Get-NewPackages {
+	$newPackages = @()
+
 	$packages | ForEach-Object {
 		if (!$installedPackages.Contains($_)) {
 			$newPackages += $_
 		}
-	}
+	}	
 
+	return $newPackages
+}
+
+function Remove-OldPackages {
 	if ($extraPackages.count -gt 0) {
 		Write-Host "`nFound extra packages not in packages.txt"
 		Write-Host "`nExtra apps:"
@@ -32,7 +35,9 @@ function Sync-Packages {
 			Invoke-Expression -Command "scoop.ps1 uninstall $($extraPackages -join ' ')"
 		}
 	}
+}
 
+function Install-NewPackages {
 	if ($newPackages.count -gt 0) {
 		Write-Host "`nFound packages to install" 
 		Write-Host "`nNew apps:"
@@ -45,6 +50,18 @@ function Sync-Packages {
 	}
 }
 
+function Sync-Packages {
+	Write-Host "Getting list of installed applications`n"
+
+	$installedPackages = Invoke-Expression "scoop.ps1 list" | Select-Object -ExpandProperty "name" 
+
+	$extraPackages = Get-ExtraPackages
+	$newPackages = Get-NewPackages
+
+	Remove-OldPackages
+	Install-NewPackages
+}
+
 function Update-Scoop {
 	Write-Host "`nUpdating all applications"
 
@@ -52,6 +69,8 @@ function Update-Scoop {
 	Invoke-Expression -Command "scoop.ps1 update *"
 	Invoke-Expression -Command "scoop.ps1 cleanup *"
 }
+
+$packages = Get-Content -Path "$PSScriptRoot\manifest.json" | ConvertFrom-Json | Select-Object -ExpandProperty "packages"
 
 Sync-Packages
 Update-Scoop
