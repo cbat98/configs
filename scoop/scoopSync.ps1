6d5636b824676ade506699566235689409a83871
Charlie Batten <charlie@charliebatten.co.uk>
2023-08-29 16:27:24 +0100

    Split scoopSync into functions

--- scoop/scoopSync.ps1
+++ scoop/scoopSync.ps1
@@ -1,50 +1,57 @@
 $packages = Get-Content -Path "$PSScriptRoot\manifest.json" | ConvertFrom-Json | Select-Object -ExpandProperty "packages"
 
-$extraPackages = @()
-$newPackages = @()
+function Sync-Packages {
+	$extraPackages = @()
+	$newPackages = @()
 
-Write-Host "Getting list of installed applications`n"
+	Write-Host "Getting list of installed applications`n"
 
-$installedPackages = Invoke-Expression "scoop.ps1 list" | Select-Object -ExpandProperty "name" 
+	$installedPackages = Invoke-Expression "scoop.ps1 list" | Select-Object -ExpandProperty "name" 
 
-$installedPackages | ForEach-Object {
-	Write-Host "$_"
+	$installedPackages | ForEach-Object {
+		Write-Host "$_"
 
-	if (!$packages.Contains($_)) {
-		$extraPackages += $_
+		if (!$packages.Contains($_)) {
+			$extraPackages += $_
+		}
+	}
+
+	$packages | ForEach-Object {
+		if (!$installedPackages.Contains($_)) {
+			$newPackages += $_
+		}
+	}
+
+	if ($extraPackages.count -gt 0) {
+		Write-Host "`nFound extra packages not in packages.txt"
+		Write-Host "`nExtra apps:"
+		Write-Host ($extraPackages -join ", ")
+
+		$removeExtraPackages = Read-Host -Prompt "`nDo you want these to be removed? (y/N)"
+		if ($removeExtraPackages -eq "y") {
+			Invoke-Expression -Command "scoop.ps1 uninstall $($extraPackages -join ' ')"
+		}
+	}
+
+	if ($newPackages.count -gt 0) {
+		Write-Host "`nFound packages to install" 
+		Write-Host "`nNew apps:"
+		Write-Host ($newPackages -join ", ")
+
+		$installNewPackages = Read-Host -Prompt "`nDo you want these to be installed? (y/N)"
+		if ($installNewPackages -eq "y") {
+			Invoke-Expression -Command "scoop.ps1 install $($newPackages -join ' ')"
+		}
 	}
 }
 
-$packages | ForEach-Object {
-	if (!$installedPackages.Contains($_)) {
-		$newPackages += $_
-	}
+function Update-Scoop {
+	Write-Host "`nUpdating all applications"
+
+	Invoke-Expression -Command "scoop.ps1 update"
+	Invoke-Expression -Command "scoop.ps1 update *"
+	Invoke-Expression -Command "scoop.ps1 cleanup *"
 }
 
-if ($extraPackages.count -gt 0) {
-	Write-Host "`nFound extra packages not in packages.txt"
-	Write-Host "`nExtra apps:"
-	Write-Host ($extraPackages -join ", ")
-
-	$removeExtraPackages = Read-Host -Prompt "`nDo you want these to be removed? (y/N)"
-	if ($removeExtraPackages -eq "y") {
-		Invoke-Expression -Command "scoop.ps1 uninstall $($extraPackages -join ' ')"
-	}
-}
-
-if ($newPackages.count -gt 0) {
-	Write-Host "`nFound packages to install" 
-	Write-Host "`nNew apps:"
-	Write-Host ($newPackages -join ", ")
-
-	$installNewPackages = Read-Host -Prompt "`nDo you want these to be installed? (y/N)"
-	if ($installNewPackages -eq "y") {
-		Invoke-Expression -Command "scoop.ps1 install $($newPackages -join ' ')"
-	}
-}
-
-Write-Host "`nUpdating all applications"
-
-Invoke-Expression -Command "scoop.ps1 update"
-Invoke-Expression -Command "scoop.ps1 update *"
-Invoke-Expression -Command "scoop.ps1 cleanup *"
+Sync-Packages
+Update-Scoop
