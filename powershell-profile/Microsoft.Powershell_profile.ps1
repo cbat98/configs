47284150fa714c41a5e36117055c4d826019a643
Charlie Batten <charlie@charliebatten.co.uk>
2023-09-11 10:06:01 +0100

    Formatted aliases

--- powershell-profile/Microsoft.Powershell_profile.ps1
+++ powershell-profile/Microsoft.Powershell_profile.ps1
@@ -6,15 +6,15 @@ $env:path += ";" + (Get-Item "Env:ProgramFiles(x86)").Value + "\Git\bin"
 $env:path += ";" + "C:\Program Files\GitHub CLI\"
 $env:path += ";" + "$configs\scoop\"
 
-Set-Alias grep findstr
-Set-Alias sln .\*.sln
-Set-Alias cb Set-Clipboard
-Set-Alias lg lazygit
-Set-Alias vim nvim.exe
-Set-Alias zip Compress-Archive
-Set-Alias unzip Expand-Archive
-Set-Alias ls eza -Option AllScope
-Set-Alias cat bat -Option AllScope
+Set-Alias -Name grep  -Value findstr
+Set-Alias -Name sln   -Value .\*.sln
+Set-Alias -Name cb    -Value Set-Clipboard
+Set-Alias -Name lg    -Value lazygit
+Set-Alias -Name vim   -Value nvim.exe
+Set-Alias -Name zip   -Value Compress-Archive
+Set-Alias -Name unzip -Value Expand-Archive
+Set-Alias -Name ls    -Value eza    -Option AllScope
+Set-Alias -Name cat   -Value bat    -Option AllScope
   
 Set-ExecutionPolicy -scope Process bypass
 
