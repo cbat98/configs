5602027fba1df2e21798677b9af20ddf7ed1b5d9
Charlie Batten <charlie@charliebatten.co.uk>
2023-11-09 17:47:58 +0000

    Use static omp invocation

--- powershell-profile/Microsoft.Powershell_profile.ps1
+++ powershell-profile/Microsoft.Powershell_profile.ps1
@@ -29,4 +29,5 @@ Set-Alias -Name cat   -Value bat    -Option AllScope
 
 # Import-Module -Name "Terminal-Icons"
 
-oh-my-posh init pwsh --config "$configs\oh-my-posh\material-edit.omp.json" | Invoke-Expression
+# oh-my-posh init pwsh --config "$configs\oh-my-posh\material-edit.omp.json" | Invoke-Expression
+(@(& 'C:/Users/Charlie/scoop/apps/oh-my-posh/current/oh-my-posh.exe' init pwsh --config='G:\Repos\configs\oh-my-posh\material-edit.omp.json' --print) -join "`n") | Invoke-Expression
