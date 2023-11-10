d82d64b3ef47966eca8b6afb6cbc09e4c41f69ce
Charlie Batten <charlie@charliebatten.co.uk>
2023-11-10 18:02:59 +0000

    Removed commented lines

--- powershell-profile/Microsoft.Powershell_profile.ps1
+++ powershell-profile/Microsoft.Powershell_profile.ps1
@@ -12,19 +12,9 @@ $configs = "$repos\configs"
 $env:path = "C:\Users\charlie\scoop\apps\notepadplusplus\current" + ";" + $env:path
 $env:path += ";" + "$configs\scoop\"
 
-# Set-Alias -Name grep  -Value findstr
-# Set-Alias -Name sln   -Value .\*.sln
-# Set-Alias -Name cb    -Value Set-Clipboard
 Set-Alias -Name lg    -Value lazygit
-# Set-Alias -Name zip   -Value Compress-Archive
-# Set-Alias -Name unzip -Value Expand-Archive
 Set-Alias -Name ls    -Value eza    -Option AllScope
 Set-Alias -Name ll    -Value Get-AllFilesAsList
 Set-Alias -Name cat   -Value bat    -Option AllScope
   
-# Set-ExecutionPolicy -scope Process bypass
-
-# Import-Module -Name "Terminal-Icons"
-
-# oh-my-posh init pwsh --config "$configs\oh-my-posh\material-edit.omp.json" | Invoke-Expression
 (@(& 'C:/Users/Charlie/scoop/apps/oh-my-posh/current/oh-my-posh.exe' init pwsh --config='G:\Repos\configs\oh-my-posh\material-edit.omp.json' --print) -join "`n") | Invoke-Expression
