74790735c64e7c649cc4aee08510aeab3694f635
Charlie Batten <charlie@charliebatten.co.uk>
2023-11-16 15:21:57 +0000

    Added oex alias

--- powershell-profile/Microsoft.Powershell_profile.ps1
+++ powershell-profile/Microsoft.Powershell_profile.ps1
@@ -6,6 +6,10 @@ function Get-AllFilesAsList {
     eza -la $directory
 }
 
+function Open-Explorer {
+    explorer .
+}
+
 $repos = "G:\Repos"
 $configs = "$repos\configs"
 
@@ -16,5 +20,6 @@ Set-Alias -Name lg    -Value lazygit
 Set-Alias -Name ls    -Value eza    -Option AllScope
 Set-Alias -Name ll    -Value Get-AllFilesAsList
 Set-Alias -Name cat   -Value bat    -Option AllScope
+Set-Alias -Name oex   -Value Open-Explorer
   
 (@(& 'C:/Users/Charlie/scoop/apps/oh-my-posh/current/oh-my-posh.exe' init pwsh --config='G:\Repos\configs\oh-my-posh\material-edit.omp.json' --print) -join "`n") | Invoke-Expression
