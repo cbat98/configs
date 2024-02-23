6a91fa3da46d01ba5498706e73130fe5d635ae0b
Charlie Batten <charlie.batten@microlise.com>
2024-02-23 15:40:41 +0000

    Renamed powershell profile folder

--- powershell/home.ps1
+++ powershell/home.ps1
@@ -1,8 +1,4 @@
 function Get-AllFilesAsList {
-    param (
-        [Parameter()][string]$directory="./"
-    )
-
     eza -la $directory
 }
 
@@ -14,13 +10,13 @@ $repos = "G:\Repos"
 $configs = "$repos\configs"
 
 $env:path = "C:\Users\charlie\scoop\apps\notepadplusplus\current" + ";" + $env:path
-$env:path += ";" + "$configs\powershell-profile\path"
+$env:path += ";" + "$configs\powershell\path"
 $env:path += ";" + "$configs\scoop\"
 
-Set-Alias -Name lg    -Value lazygit
-Set-Alias -Name ls    -Value eza    -Option AllScope
-Set-Alias -Name ll    -Value Get-AllFilesAsList
-Set-Alias -Name cat   -Value bat    -Option AllScope
-Set-Alias -Name oex   -Value Open-Explorer
-  
+Set-Alias -Name lg     -Value lazygit
+Set-Alias -Name ls     -Value eza    -Option AllScope
+Set-Alias -Name cat    -Value bat    -Option AllScope
+Set-Alias -Name ll     -Value Get-AllFilesAsList
+Set-Alias -Name oex    -Value Open-Explorer
+
 (@(& 'C:/Users/Charlie/scoop/apps/oh-my-posh/current/oh-my-posh.exe' init pwsh --config='G:\Repos\configs\oh-my-posh\material-edit.omp.json' --print) -join "`n") | Invoke-Expression
