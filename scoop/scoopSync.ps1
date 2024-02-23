15286bcd1a7811cdd0b75f19884a9d7374eeae55
Charlie Batten <charlie.batten@microlise.com>
2024-02-23 15:11:15 +0000

    Call apps and bucket files from main

--- scoop/scoopSync.ps1
+++ scoop/scoopSync.ps1
@@ -1,91 +1,18 @@
-function Add-Buckets {
-	foreach ($bucket in $buckets) {
-		Invoke-Expression -Command "scoop.ps1 bucket add $bucket"
-	}
-}
+. $PSScriptRoot\buckets.ps1
+. $PSScriptRoot\apps.ps1
 
-function Get-ExtraPackages {
-	$extraPackages = @()
-
-	foreach($package in $installedPackages) {
- 		Write-Host "$package"
-
-		if (!$packages.Contains($package)) {
-			$extraPackages += $package
-		}
-	}
-
-	return $extraPackages
-}
-
-function Get-NewPackages {
-	$newPackages = @()
-
-	foreach ($package in $packages) {
-		if (!$installedPackages.Contains($package)) {
-			$newPackages += $package
-		}
-	}	
-
-	return $newPackages
-}
-
-function Remove-ExtraPackages {
-	if ($extraPackages.count -gt 0) {
-		Write-Host "`nFound extra packages not in manifest.json"
-		Write-Host "`nExtra apps:"
-		Write-Host ($extraPackages -join ", ")
-
-		$removeExtraPackages = Read-Host -Prompt "`nDo you want these to be removed? (y/N)"
-		if ($removeExtraPackages -eq "y") {
-			Invoke-Expression -Command "scoop.ps1 uninstall $($extraPackages -join ' ')"
-		}
-	}
-}
-
-function Install-NewPackages {
-	if ($newPackages.count -gt 0) {
-		Write-Host "`nFound packages to install" 
-		Write-Host "`nNew apps:"
-		Write-Host ($newPackages -join ", ")
-
-		$installNewPackages = Read-Host -Prompt "`nDo you want these to be installed? (y/N)"
-		if ($installNewPackages -eq "y") {
-			Invoke-Expression -Command "scoop.ps1 install $($newPackages -join ' ')"
-		}
-	}
-}
-
-function Sync-Packages {
-	Write-Host "Getting list of installed applications`n"
-
-	$installedPackages = Invoke-Expression "scoop.ps1 list" | Select-Object -ExpandProperty "name" 
-
-	$extraPackages = Get-ExtraPackages
-	$newPackages = Get-NewPackages
-
-	Remove-ExtraPackages
-	Install-NewPackages
-}
-
-function Update-Scoop {
-	Write-Host "`nUpdating all applications"
-
-	Invoke-Expression -Command "scoop.ps1 update"
-	Invoke-Expression -Command "scoop.ps1 update *"
-	Invoke-Expression -Command "scoop.ps1 cleanup *"
-}
-
-$manifestPath = "$PSScriptRoot\manifest.json"
+$manifestFileName = "manifest.json"
+$manifestPath = Join-Path -Path $PSScriptRoot -ChildPath $manifestFileName
 $manifest = Get-Content -Path $manifestPath | ConvertFrom-Json
 
 $hostname = $env:COMPUTERNAME
 $commonManifest = $manifest.hosts.common
 $hostManifest = $manifest.hosts | Select-Object -ExpandProperty $hostname
 
-$buckets = ($commonManifest.buckets + $hostManifest.buckets) | Sort-Object -Unique
-$packages = ($commonManifest.packages + $hostManifest.packages) | Sort-Object -Unique
+$targetBuckets = ($commonManifest.buckets + $hostManifest.buckets) | Sort-Object -Unique
+$targetApps = ($commonManifest.apps + $hostManifest.apps) | Sort-Object -Unique
 
-Add-Buckets
-Sync-Packages
-Update-Scoop
+Write-Host "--ScoopSync--"
+
+Sync-Buckets -TargetBuckets $targetBuckets
+Sync-Apps -TargetApps $targetApps
