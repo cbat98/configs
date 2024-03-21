0383a3d48c0d8f71612488e529b38b56fbfdf946
Charlie Batten <charlie@charliebatten.co.uk>
2024-03-21 09:22:37 +0000

    Added -all to git fetch

--- powershell/path/Fetch-AllRepositories.ps1
+++ powershell/path/Fetch-AllRepositories.ps1
@@ -14,15 +14,21 @@ $repos = Invoke-Expression -Command "$($PSCommandPath | Split-Path -Parent)\Get-
 Write-Host "Found $($repos.Count) repositories"
 
 $repos | ForEach-Object -Process {
-    if ($Prune) {
-        Write-Host "Running 'git fetch --prune' in $_"
-        Invoke-Expression -Command "git -C $_ fetch --prune"
-    } else {
-        Write-Host "Running 'git fetch' in $_"
-        Invoke-Expression -Command "git -C $_ fetch"
+    if ($Prune)
+    {
+        Write-Host "Running 'git fetch -all --prune' in $_"
+        Invoke-Expression -Command "git -C $_ fetch --all --prune"
+    } else
+    {
+        Write-Host "Running 'git fetch -all' in $_"
+        Invoke-Expression -Command "git -C $_ fetch --all"
     }
 
-    $errors = if ($lastexitcode -ne 0) { $errors + 1 } else { $errors }
+    $errors = if ($lastexitcode -ne 0)
+    { $errors + 1 
+    } else
+    { $errors 
+    }
 }
 
 $outputString = (Get-Date -UFormat "%Y-%m-%d - %H-%M-%S").ToString() + " - fetched $($repos.Count) repositories with $errors errors"
