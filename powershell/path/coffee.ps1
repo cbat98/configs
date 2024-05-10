6814f79a91249cd75d25622887c5ba972c58c46c
Charlie Batten <charlie.batten@microlise.com>
2024-05-10 11:40:25 +0100

    Added brb msg to coffee.ps1

--- powershell/path/coffee.ps1
+++ powershell/path/coffee.ps1
@@ -10,7 +10,7 @@ write-host @'
    |             | )  )
    |             |/  /
    |             /  /    coffee break
-   |            (  /
+   |            (  /         brb
    \             y'
     `-.._____..-'
 '@
