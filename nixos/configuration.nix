b9e566b4ee4c5a4a78d0a3e91f1ce876c4536dc5
Charlie B <charlie@charliebatten.co.uk>
2024-09-11 21:43:40 +0100

    Consolidate network settings

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -21,8 +21,9 @@
     "flakes"
   ];
 
-  networking.hostName = "charlie-nixlt"; # Define your hostname.
   networking = {
+    hostName = "charlie-nixlt";
+    networkmanager.enable = true;
     nameservers = [
       "1.1.1.1"
       "1.0.0.1"
@@ -30,14 +31,12 @@
       "8.8.4.4"
     ];
   };
+
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
   # Configure network proxy if necessary
   # networking.proxy.default = "http://user:password@proxy:port/";
   # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
-
-  # Enable networking
-  networking.networkmanager.enable = true;
 
   # Set your time zone.
   time.timeZone = "Europe/London";
