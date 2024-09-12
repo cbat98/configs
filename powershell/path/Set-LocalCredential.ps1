54aa66ff6da224a203b65460ab9e990a575c658a
Charlie Batten <charlie.batten@microlise.com>
2024-09-12 17:06:41 +0100

    Remove set-localcredential

--- powershell/path/Set-LocalCredential.ps1
+++
@@ -1,7 +0,0 @@
-param (
-    [Parameter(Mandatory=$true)][string]$credName
-)
-
-$inFile = "${env:\userprofile}\credentials\$credName.creds"
-$Credential = Get-Credential
-$Credential | Export-CliXml -Path $inFile
