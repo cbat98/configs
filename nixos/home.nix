533467d47191966d0cb2d3760d6d4b2e8e8ff950
Charlie B <charlie@charliebatten.co.uk>
2024-09-12 18:13:11 +0100

    Set git default branch to `main`

--- nixos/home.nix
+++ nixos/home.nix
@@ -90,6 +90,7 @@ in
           user.name = "Charlie B";
           user.email = "charlie@charliebatten.co.uk";
           pull.rebase = true;
+          init.defaultBranch = "main";
         };
       };
       kitty = {
