2f7a11d19c700bd32e0e495e3dcacebb1ce2fdd8
Charlie B <charlie@charliebatten.co.uk>
2024-11-25 15:15:02 +0000

    Added gestures,device

--- nixos/home.nix
+++ nixos/home.nix
@@ -95,6 +95,13 @@ in {
             natural_scroll = false;
           };
         };
+        gestures = {
+          workspace_swipe = false;
+        };
+        device = {
+          name = "epic-mouse-v1";
+          sensitivity = -0.5;
+        };
       };
     };
     home = {
