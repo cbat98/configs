5ab6886b1992c73d9a31d9ece91049cdace86865
Charlie B <charlie@charliebatten.co.uk>
2024-11-26 16:55:37 +0000

    Configure waybar

--- nixos/home.nix
+++ nixos/home.nix
@@ -183,7 +183,6 @@ in {
         ripgrep
         tldr
         unzip
-        waybar
         wl-clipboard
         wofi
       ];
@@ -240,6 +239,43 @@ in {
           background_opacity 0.8
           enable_audio_bell no
         '';
+      };
+      waybar = {
+        enable = true;
+        settings = {
+          mainBar = {
+            layer = "top";
+            position = "top";
+            height = 30;
+            output = [
+              "eDP-1"
+              "HDMI-A-1"
+            ];
+            modules-left = [
+              "hyprland/workspaces"
+              "hyprland/submap"
+            ];
+            modules-center = [
+              "hyprland/window"
+            ];
+            modules-right = [
+              "temperature"
+            ];
+
+            "sway/workspaces" = {
+              disable-scroll = true;
+              all-outputs = true;
+            };
+            "custom/hello-from-waybar" = {
+              format = "hello {}";
+              max-length = 40;
+              interval = "once";
+              exec = pkgs.writeShellScript "hello-from-waybar" ''
+                echo "from within waybar"
+              '';
+            };
+          };
+        };
       };
     };
   };
