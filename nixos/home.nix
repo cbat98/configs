9b78ae6c90bef71f51115f904535ccea0ec40671
Charlie B <charlie@charliebatten.co.uk>
2024-11-26 14:37:08 +0000

    Added window rules

--- nixos/home.nix
+++ nixos/home.nix
@@ -120,6 +120,10 @@ in {
           ", XF86AudioPlay, exec, playerctl play-pause"
           ", XF86AudioPrev, exec, playerctl previous"
         ];
+        windowrulev2 = [
+          "suppressevent maximize, class:.*"
+          "nofocus,class:^$,title:^$,xwayland:1,floating:1,fullscreen:0,pinned:0"
+        ];
       };
     };
     home = {
