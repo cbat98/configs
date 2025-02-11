63f56118eb2244c5b99b24dbc75dfcab18175f27
Charlie Batten <charlie.batten@microlise.com>
2025-02-11 08:57:23 +0000

    Add install skip on profiler if installed

--- powershell/path/ProfileScriptBlock.ps1
+++ powershell/path/ProfileScriptBlock.ps1
@@ -2,6 +2,11 @@ param (
     [Parameter(Mandatory)][scriptblock]$ScriptBlock
 )
 
-Install-Module -Name Profiler
+$moduleName = "Profiler"
+if (-not (Get-Module -Name $moduleName -ListAvailable)) {
+    Install-Module -Name Profiler
+}
+
+Import-Module -Name $moduleName
 
 Trace-Script -ScriptBlock $ScriptBlock | Select-Object -ExpandProperty Top50SelfDuration | Out-GridView
