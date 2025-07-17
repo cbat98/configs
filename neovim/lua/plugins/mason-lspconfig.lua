745d0ea5ed27cffece8002b2fdf77cd92f3fd859
Charlie Batten <charlie.batten@microlise.com>
2025-07-17 16:59:52 +0100

    Automatically enable lsps

--- neovim/lua/plugins/mason-lspconfig.lua
+++ neovim/lua/plugins/mason-lspconfig.lua
@@ -8,9 +8,6 @@ return {
         {
             'neovim/nvim-lspconfig',
             init = function()
-                vim.lsp.enable('lua_ls')
-                vim.lsp.enable('powershell_es')
-
                 vim.lsp.config('lua_ls', {
                     settings = {
                         Lua = {
@@ -22,5 +19,6 @@ return {
                 })
             end,
         }
-    }
+    },
+    opts = {}
 }
