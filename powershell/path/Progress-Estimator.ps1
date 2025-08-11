afcca7da411d3df5c99a1373f0b7377c0903ca29
Charlie Batten <charlie.batten@microlise.com>
2025-08-11 13:56:00 +0100

    Add progress estimator script

---
+++ powershell/path/Progress-Estimator.ps1
@@ -0,0 +1,21 @@
+param (
+    [Parameter(Mandatory, HelpMessage = "The time that the 'job' started at in yy-MM-dd HH:mm:ss format")]
+    [string]$startedAt,
+    [Parameter(Mandatory, HelpMessage = "How far through the 'job' is as a percentage (0 to 1)")]
+    [double]$percentZeroOne
+)
+
+$startDateTime = [datetime]::parseexact($startedAt, 'yy-MM-dd HH:mm:ss', $null)
+$nowDateTime = Get-Date
+
+$elapsed = $nowDateTime - $startDateTime
+
+$totalSeconds = $elapsed.TotalSeconds / $percentZeroOne
+
+$secondsLeft = $totalSeconds - $elapsed.TotalSeconds
+$projComplete = (Get-Date).AddSeconds($secondsLeft).ToString("yy-MM-dd HH:mm:ss")
+
+Write-Host "Completed: $([math]::Round($percentZeroOne * 100, 2))%"
+
+Write-Host "`nElapsed time: $elapsed"
+Write-Host "Estimated completion at $projComplete"
