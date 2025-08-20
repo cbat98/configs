ed1ca7a23d62461dff5970b022961b07c8bade60
Charlie Batten <charlie.batten@microlise.com>
2025-08-20 09:23:56 +0100

    Remove legacy configs

--- winget/WinConf.ps1
+++
@@ -1,6 +0,0 @@
-#requires -RunAsAdministrator
-param (
-    [Parameter()][string]$ConfigurationFilePath="$PSScriptRoot\configuration.dsc.yaml"
-)
-
-Invoke-Expression -Command "winget configure -f '$ConfigurationFilePath' --accept-configuration-agreements --disable-interactivity --verbose"
