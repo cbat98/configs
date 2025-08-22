1035775df2a8703f2e7b63d4f64b10e2d35e5557
Charlie Batten <charlie.batten@microlise.com>
2025-08-22 14:37:53 +0100

    Formatting

--- neovim/lua/plugins/snacks.lua
+++ neovim/lua/plugins/snacks.lua
@@ -14,12 +14,12 @@ return {
         quickfile = {},
     },
     keys = {
-        {"<leader>sp", function() require("snacks").picker() end, desc = "[S]earch [P]ickers" },
-        {"<leader>sf", function() require("snacks").picker.files() end, desc = "[S]earch [F]iles"},
-        {"<leader>sg", function() require("snacks").picker.grep() end, desc = "[S]earch [G]rep"},
-        {"<leader>sd", function() require("snacks").picker.diagnostics() end, desc = "[S]earch [D]iagnostics"},
-        {"<leader>sr", function() require("snacks").picker.resume() end, desc = "[S]earch [R]esume"},
-        {"<leader>ss", function() require("snacks").picker.lsp_symbols() end, desc = "[S]earch [S]ymbols"},
-        {"<leader>sk", function() require("snacks").picker.keymaps() end, desc = "[S]earch [K]eymaps"}
+        { "<leader>sp", function() require("snacks").picker() end,             desc = "[S]earch [P]ickers" },
+        { "<leader>sf", function() require("snacks").picker.files() end,       desc = "[S]earch [F]iles" },
+        { "<leader>sg", function() require("snacks").picker.grep() end,        desc = "[S]earch [G]rep" },
+        { "<leader>sd", function() require("snacks").picker.diagnostics() end, desc = "[S]earch [D]iagnostics" },
+        { "<leader>sr", function() require("snacks").picker.resume() end,      desc = "[S]earch [R]esume" },
+        { "<leader>ss", function() require("snacks").picker.lsp_symbols() end, desc = "[S]earch [S]ymbols" },
+        { "<leader>sk", function() require("snacks").picker.keymaps() end,     desc = "[S]earch [K]eymaps" }
     }
 }
