9bdcf463abd069e3d1e2f3ba6547a3913f10e552
cbat98 <charlie@charliebatten.co.uk>
2025-11-06 12:14:48 +0000

    Add user config

--- nixos/p3561/configuration.nix
+++ nixos/p3561/configuration.nix
@@ -141,6 +141,19 @@ in {
     };
   };
 
+  #
+  # --- User Configuration -------------------------------------
+  #
+
+  users = {
+    users.charlie = {
+      isNormalUser = true;
+      extraGroups = [
+        "wheel"
+      ];
+    };
+  };
+
   # Copy the NixOS configuration file and link it from the resulting system
   # (/run/current-system/configuration.nix). This is useful in case you
   # accidentally delete configuration.nix.
