b25a3f71d7167667cdc62ca2d5e3516439f14a28
cbat98 <charlie@charliebatten.co.uk>
2026-01-12 15:51:24 +0000

    Update waybar config

--- nixos/flake/modules/home-manager/waybar.nix
+++ nixos/flake/modules/home-manager/waybar.nix
@@ -7,124 +7,147 @@
       * {
         border: none;
         border-radius: 0;
-
+        font-family: "CaskaydiaCove Nerd Font";
         font-size: 14px;
         min-height: 0;
       }
       window#waybar {
-        background: #272822;
+        background: rgba(39, 40, 34, 0.9);
         color: #f8f8f2;
-        opacity: 0.9;
       }
       #workspaces {
         background: #3e3d32;
-        color: #f8f8f2;
-        padding: 0px 10px;
-        margin: 3px 5px;
+        margin: 4px 4px;
+        padding: 0px 5px;
         border-radius: 10px;
       }
       #workspaces button {
         padding: 0px 5px;
         color: #75715e;
-        background: transparent;
-        border: none;
-        border-radius: 5px;
       }
       #workspaces button.active {
         color: #f8f8f2;
       }
-      #workspaces button.occupied {
-        color: #a6e22e;
+      #workspaces button.urgent {
+        color: #f92672;
       }
       #window {
-        background: transparent;
-        color: #f8f8f2;
+        margin: 4px 4px;
         padding: 0px 10px;
-        margin: 3px 5px;
-      }
-      #pulseaudio, #network, #battery, #clock, #cpu, #memory, #idle_inhibitor, #custom-updates, #tray {
         background: #3e3d32;
-        color: #f8f8f2;
+        border-radius: 10px;
+      }
+      #network, #pulseaudio, #backlight, #battery, #cpu, #memory, #clock, #tray {
         padding: 0px 10px;
-        margin: 3px 3px;
+      }
+      .modules-right > widget > box {
+        background: #3e3d32;
+        margin: 4px 2px;
+        padding: 0px 5px;
         border-radius: 10px;
       }
       #pulseaudio.muted {
-          color: #f92672;
-      }
-      #battery.discharging {
-        color: #f8f8f2;
+        color: #f92672;
       }
       #battery.warning {
         color: #e6db74;
       }
       #battery.critical:not(.charging) {
-          color: #f92672;
+        color: #f92672;
+      }
+      #clock {
+        color: #a6e22e;
       }
     '';
     settings = {
       mainBar = {
-        spacing = 10;
+        spacing = 4;
         layer = "top";
         position = "top";
-        height = 30;
-        output = [
-          "*"
-        ];
-        modules-left = [
-          "hyprland/workspaces"
-        ];
-        modules-center = [
-          "hyprland/window"
-        ];
+        height = 34;
+        modules-left = [ "hyprland/workspaces" ];
+        modules-center = [ "hyprland/window" ];
         modules-right = [
-          "network"
-          "pulseaudio"
-          "battery"
-          "cpu"
-          "memory"
+          "group/system"
+          "group/hardware"
           "clock"
           "tray"
         ];
+
+        "group/system" = {
+          orientation = "horizontal";
+          modules = [
+            "network"
+            "pulseaudio"
+            "backlight"
+            "battery"
+          ];
+        };
+
+        "group/hardware" = {
+          orientation = "horizontal";
+          modules = [
+            "cpu"
+            "memory"
+          ];
+        };
+
+        "hyprland/workspaces" = {
+          on-click = "activate";
+          format = "{name}";
+        };
+
+        "network" = {
+          format-wifi = "   {essid}";
+          format-ethernet = "󰈀   {ipaddr}";
+          format-disconnected = "󰤮   Disconnected";
+          tooltip-format = "{ifname} via {gwaddr}";
+        };
+
+        "pulseaudio" = {
+          format = "{icon}   {volume}%";
+          format-muted = "󰝟   MUTE";
+          format-icons = {
+            default = [ "󰕿" "󰖀" "󰕾" ];
+          };
+          on-click = "pavucontrol";
+        };
+
+        "backlight" = {
+          format = "{icon}   {percent}%";
+          format-icons = [ "󰃞" "󰃟" "󰃠" ];
+        };
+
         "battery" = {
-          format = " {capacity}%";
-          format-charging = " {capacity}%";
-          format-plugged = " {capacity}%";
-          format-alt = "{time}";
           states = {
             warning = 30;
             critical = 15;
           };
-          weighted-average = true;
+          format = "{icon}   {capacity}%";
+          format-charging = "󰂄   {capacity}%";
+          format-plugged = "󰂄   {capacity}%";
+          format-icons = [ "󰁺" "󰁻" "󰁼" "󰁽" "󰁾" "󰁿" "󰂀" "󰂁" "󰂂" "󰁹" ];
         };
-        "clock" = {
-          interval = 1;
-          format = "󰥔 {:%d/%m/%Y %H:%M:%S}";
-          tooltip = false;
-        };
-        "network" = {
-          format = "󰤨 {ipaddr} ({essid})";
-        };
-        "pulseaudio" = {
-          format = "  {volume}%";
-          format-muted = " MUTE";
-          on-click = "pavucontrol";
-        };
-        "hyprland/workspaces" = {
-          disable-scroll = true;
-          active-only = false;
-        };
+
         "cpu" = {
-          format = "󰍛 {usage}%";
-          interval = 1;
+          format = "󰍛   {usage}%";
+          interval = 2;
         };
+
         "memory" = {
-          format = "󰍈 {percentage}%";
-          interval = 1;
+          format = "󰍈   {percentage}%";
+          interval = 2;
         };
+
+        "clock" = {
+          format = "󰥔   {:%H:%M}";
+          format-alt = "󰃭   {:%d/%m/%Y}";
+          tooltip-format = "<big>{:%Y %B}</big>\n<tt><small>{calendar}</small></tt>";
+        };
+
         "tray" = {
-          "icon-size" = 18;
-          "spacing" = 9;
+          icon-size = 21;
+          spacing = 10;
         };
       };
     };
