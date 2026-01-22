1120a87f058c8d1c4e191b1ceadf70354f43bfda
Charlie Batten <charlie.batten@microlise.com>
2026-01-22 17:09:09 +0000

    Add native pwsh equivalents

--- powershell/home.ps1
+++ powershell/home.ps1
@@ -29,6 +29,24 @@ function Start-CleanNeovim {
     Invoke-Expression -Command "nvim --clean $args"
 }
 
+function Remove-TrailingWhitespace {
+    param (
+        [Parameter()][string]$Path = "."
+    )
+
+    Get-ChildItem -Path $Path -Recurse -File | Where-Object { $_.FullName -notmatch '\\\.git\\' } | ForEach-Object {
+        $file = $_.FullName
+        $content = Get-Content $file
+        if ($null -ne $content) {
+            $trimmed = $content | ForEach-Object { $_.TrimEnd(" `t") }
+            # Only write if there's a difference
+            if (-not ($content -join "`n" -ceq ($trimmed -join "`n"))) {
+                $trimmed | Set-Content $file
+            }
+        }
+    }
+}
+
 function Git-Wrapper {
     param (
         [Parameter()][string]$Command
@@ -48,6 +66,7 @@ Set-Alias -Name npp -Value "C:\Program Files\Notepad++\notepad++.exe"
 Set-Alias -Name gitopen -Value Open-GitRemoteRepo
 Set-Alias -Name gs -Value Git-Status
 Set-Alias -Name g -Value Git-Wrapper
+Set-Alias -Name tw -Value Remove-TrailingWhitespace
 
 if (Get-Command -Name "nvim") {
     Set-Alias -Name vim -Value Start-CleanNeovim
