ab7bdfa5db95dbdd53490b762f0b7130860cf5a8
Charlie Batten <charlie.batten@microlise.com>
2026-02-16 13:47:59 +0000

    Config origami

--- neovim/lua/plugins/origami.lua
+++ neovim/lua/plugins/origami.lua
@@ -1,7 +1,13 @@
 return {
 	"chrisgrieser/nvim-origami",
 	event = "VeryLazy",
-	opts = {},
+	opts = {
+        foldKeymaps = {
+            setup = true,
+            closeOnlyOnFirstColumn = true,
+            scrollLeftOnCaret = false,
+	    },
+    },
 	init = function()
 		vim.opt.foldlevel = 99
 		vim.opt.foldlevelstart = 99
