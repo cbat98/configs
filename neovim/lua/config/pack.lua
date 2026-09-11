19d56725b8eea4fbc109637388cc20fdf04b10ae
Charlie Batten <charlie.batten@microlise.com>
2026-09-11 09:30:49 +0100

    Add lualine and modicator neovim plugins

--- neovim/lua/config/pack.lua
+++ neovim/lua/config/pack.lua
@@ -30,6 +30,9 @@ local specs = {
   { src = "https://codeberg.org/andyg/leap.nvim" },
   { src = gh("stevearc/oil.nvim") },
   { src = gh("lewis6991/gitsigns.nvim") },
+  { src = gh("nvim-tree/nvim-web-devicons") },
+  { src = gh("nvim-lualine/lualine.nvim") },
+  { src = gh("mawkler/modicator.nvim") },
 }
 
 vim.pack.add(specs, { load = true, confirm = false })
